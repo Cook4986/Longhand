29c44948a6b5c07cc2b3a4df71d9c56c57a592fe
Mona Lisa <matt@mncook.net>
2022-03-25 08:53:30 -0400

    timestamp output

--- Longhand_exporter.py
+++ Longhand_exporter.py
@@ -3,32 +3,20 @@ import bpy
 import time
 import os
 
+secs = time.localtime()
+
+#declarations
 bpy.ops.object.select_all(action='SELECT')
 view_layer = bpy.context.view_layer
 obj_active = view_layer.objects.active
 selection = bpy.context.selected_objects
 
-
 for obj in selection:
-
     obj.select_set(True)
-
-    # some exporters only use the active object
     view_layer.objects.active = obj
-
-    name = bpy.path.clean_name(obj.name)
-    fn = "/Users/matthewcook/Desktop/out5"
-
+    fn = ".../Longhand/outputs" + str(time.asctime(secs))
     bpy.ops.export_scene.gltf(filepath=fn + ".GLB", use_visible=True)
-
     obj.select_set(False)
-
     print("written:", fn)
-
-
-view_layer.objects.active = obj_active
-
-for obj in selection:
-    obj.select_set(True)
 
 print("have a nice day")
