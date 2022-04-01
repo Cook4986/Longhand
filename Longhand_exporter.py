0011a336a3ffa73b6aec46c5df7b3c42e2d8a3ab
Mona Lisa <matt@mncook.net>
2022-04-01 09:13:07 -0400

    minor changes

--- Longhand_exporter.py
+++ Longhand_exporter.py
@@ -10,12 +10,12 @@ bpy.ops.object.select_all(action='SELECT')
 view_layer = bpy.context.view_layer
 obj_active = view_layer.objects.active
 selection = bpy.context.selected_objects
+outPath = ".../outputs" + str(time.asctime(secs))
 
 for obj in selection:
     obj.select_set(True)
     view_layer.objects.active = obj
-    fn = ".../Longhand/outputs" + str(time.asctime(secs))
-    bpy.ops.export_scene.gltf(filepath=fn + ".GLB", use_visible=True)
+    bpy.ops.export_scene.gltf(filepath=outPath + ".GLB", use_visible=True)
     obj.select_set(False)
     print("written:", fn)
 
