894da1613abf8237e620464c4e7c4fa7416d2aca
Mona Lisa <matt@mncook.net>
2022-05-31 11:34:30 -0400

    normalize model sizes prior to rescale and reorient

--- Longhand_aligner.py
+++ Longhand_aligner.py
@@ -1,26 +1,33 @@
 #Takes Blender scene of downloaded Sketchfab files and resizes then randomly distributes models according to their frequency in text corpus
 #launch blender with terminal: !/Applications/Blender.app/Contents/MacOS/Blender
 import bpy
+from bpy import context
 import json
 
 #declarations
-input = "....txt"
+input = "...objects.txt"
 count = 0
 
 #enable critical add-ons
 bpy.ops.preferences.addon_enable(module="space_view3d_align_tools")
+bpy.ops.preferences.addon_enable(module="add_curve_extra_objects")
+
 
 #create dictionary from objects document
 with open(input,'r') as f: 
     models = json.load(f)
 {key: value for key, value in sorted(models.items(), key=lambda item: item[1])}
 
-#create temporary cube and spiral objects for scaling and model distribution
+#create spiral object for model distribution
 bpy.ops.curve.spirals(spiral_type='SPHERE',radius=(50))
 bpy.context.object.scale[2] = 0.25
 bpy.context.object.location[2] = 15
 bpy.context.object.location[0] = 20.17
-bpy.ops.mesh.primitive_cube_add(size=2, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(10, 10, 10))
+
+#create ground plane + rescale and align downloads to ground plane
+bpy.ops.mesh.primitive_plane_add(size=100, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+bpy.ops.object.select_all(action='SELECT')
+bpy.ops.object.select_all(action='DESELECT')
 
 #rescale and distributedownloads according to their relative frequency in corpus
 for key,value in models.items():
@@ -31,8 +38,58 @@ for key,value in models.items():
         freq = (value[0])
         Object.select_set(True)
         
-        #rescale to fit reference cube
-        bpy.ops.object.align_tools(fit_x=True, fit_y=True, fit_z=True)
+        #normalize scale script from Athanaze (https://github.com/Athanaze/Blender-Normalize-Object)
+        context.scene.cursor.location = [0.0, 0.0, 0.0]
+        bpy.ops.object.rotation_clear(clear_delta=False)
+        bpy.ops.object.location_clear(clear_delta=False)
+        bpy.ops.object.scale_clear(clear_delta=False)
+
+        obj = context.active_object
+        v = obj.data.vertices
+
+        highest = [v[0].co[0], v[0].co[1], v[0].co[2]]
+        lowest = [v[0].co[0], v[0].co[1], v[0].co[2]]
+
+        for v in obj.data.vertices:
+            c = v.co
+            
+            if c[0] > highest[0]:
+                highest[0] = c[0]
+            
+            if c[0] < lowest[0]:
+                lowest[0] = c[0]
+            
+            if c[1] > highest[1]:
+                highest[1] = c[1]
+            
+            if c[1] < lowest[1]:
+                lowest[1] = c[1]
+            
+            if c[2] > highest[2]:
+                highest[2] = c[2]
+            
+            if c[2] < lowest[2]:
+                lowest[2] = c[2]
+
+        bpy.ops.object.origin_set(type='ORIGIN_CURSOR', center='MEDIAN')
+
+        size = [highest[0]-lowest[0], highest[1]-lowest[1],highest[2]-lowest[2]]
+
+        s = 2/sorted(size)[2]
+
+        bpy.ops.transform.resize(
+            value=(s, s, s),
+            orient_type='GLOBAL',
+            orient_matrix=((1, 0, 0),(0, 1, 0), (0, 0, 1)),
+            orient_matrix_type='GLOBAL',
+            mirror=True,
+            use_proportional_edit=False,
+            proportional_edit_falloff='SMOOTH',
+            proportional_size=1,
+            use_proportional_connected=False,
+            use_proportional_projected=False
+        )
+        #rescale by relative frequency in corpus
         Object.scale[0] = .1 * freq
         Object.scale[1] = .1 * freq
         Object.scale[2] = .1 * freq
@@ -49,18 +106,6 @@ for key,value in models.items():
         print((str(Object)) + "rescaled to: " + (str(Object.scale)) + " at " + (str(Object.location)))
         bpy.ops.object.select_all(action='DESELECT')
         count = count + 1
-        
-#remove scaling cube
-bpy.ops.object.select_all(action='DESELECT')
-bpy.data.objects["Cube.001"].select_set(True)
-bpy.ops.object.delete()
-#remove curves
-
-#create ground plane + rescale and align downloads to ground plane
-bpy.ops.mesh.primitive_plane_add(size=100, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
-bpy.ops.object.select_all(action='SELECT')
-#bpy.ops.object.align_tools(loc_z=True, ref1='0', ref2='0')
-bpy.ops.object.select_all(action='DESELECT')
 
 #add Hubs components to surface plane
 bpy.data.objects["Plane"].select_set(True)
